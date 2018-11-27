68fbaf98bdef608a2cac2b45cf7179411ae15180
Julien Longayrou <julien.longayrou@epitech.eu>
2018-11-27 23:08:28 +0100

    Update Paths.js

--- src/utils/Paths.js
+++ src/utils/Paths.js
@@ -1,10 +1,9 @@
 const Paths = {
 
-    SIRET_API: "https://www.numero-de-siret.com/api",
-
     SIRET: "/siret",
 
-    HOST: "http://91.121.155.83:8083",
+    //HOST: "http://91.121.155.83:8083",
+    HOST: "http://localhost:8083",
 
     PASSWORD: "/password",
 
